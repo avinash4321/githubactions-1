9afc753e1fe2f60c07cfbff57a549e63573c3804
Avinash <41838739+avinash4321@users.noreply.github.com>
2024-02-17 23:26:03 +0530

    Multi-job with needs

--- ascii-script.sh
+++ ascii-script.sh
@@ -2,6 +2,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a dragon ...... Mooooo" >> dragon.txt
-grep -i "dragon" dragon.txt        
-cat dragon.txt
-ls -ltra
+# grep -i "dragon" dragon.txt        
+# cat dragon.txt
+# ls -ltra
